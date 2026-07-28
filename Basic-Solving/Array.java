b31232f4d3e29e1b021466f73c8f3df667f3e309
Darshan Sunil Kaware <darshan.kaware.comp.25@vpkbiet.org>
2026-07-28 23:14:18 +0530

    Solving

---
+++ Basic-Solving/Array.java
@@ -0,0 +1,26 @@
+class Array{
+    public static void printArray(int arr[]){
+        for(int i=0; i<arr.length; i++ ){
+            System.out.print(arr[i]+ " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]){
+        int arr[]={1, 4, 5, 22,6,7,};
+
+        for(int i=0; i<arr.length-1; i++){
+
+            for(int j=0; j<arr.length-i-1; j++){
+
+                if(arr[j]>arr[j+1]){
+
+                    int temp =arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        printArray(arr);
+    }
+}
