0f34dc3c1557a1b09f4e7bc232e1207e99914fcf
Darshan Sunil Kaware <darshan.kaware.comp.25@vpkbiet.org>
2026-08-03 19:26:14 +0530

    Recursion in Java

---
+++ Recursion/Recursion.java
@@ -0,0 +1,13 @@
+public class Recursion {
+    public static void printNum(int n){
+        if(n==100){
+            return;
+        }
+        System.out.println(n);
+        printNum(n+1);
+    }
+    public static void main(String args[]){
+        int n =1;
+        printNum(n);
+    }
+}
